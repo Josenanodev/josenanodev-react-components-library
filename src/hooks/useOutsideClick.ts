67f1e8f36618ac863b156e8549d029c91b197a97
Josenanodev <73077179+Josenanodev@users.noreply.github.com>
2023-01-08 21:39:51 -0600

    Create useOutsideClick.ts

---
+++ src/hooks/useOutsideClick.ts
@@ -0,0 +1,25 @@
+import { useEffect } from "react";
+
+/**
+ * Hook that alerts clicks outside of the passed ref
+ */
+function useOutsideAlerter(elementRef: React.MutableRefObject<HTMLElement | null>, onOutsideClickAction: Function) {
+  useEffect(() => {
+    /**
+     * Alert if clicked on outside of element
+     */
+    function handleClickOutside(event) {
+      if (elementRef.current && !elementRef.current.contains(event.target)) {
+        onOutsideClickAction();
+      }
+    }
+    // Bind the event listener
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      // Unbind the event listener on clean up
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [elementRef, onOutsideClickAction]);
+}
+
+export default useOutsideAlerter;
