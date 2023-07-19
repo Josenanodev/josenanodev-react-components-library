d497012def67e1dfc5cabcafc25b3fab2ce19a2f
Jose Carlos Cardenas Martinez <Josenanodev@gmail.com>
2023-07-19 15:24:18 -0600

    JSDoc useIntersectionObserver

--- src/hooks/useIntersectionObserver.ts
+++ src/hooks/useIntersectionObserver.ts
@@ -1,5 +1,11 @@
 import { RefObject, useEffect, useState, useRef } from "react";
 
+/**
+ * Hook to observe an element's intersection
+ * @param ref ref to the element to observe
+ * @param options intersection observer options
+ * @returns a boolean indicating if the element is intersecting
+ */
 function useIntersectionObserver(
   ref: RefObject<Element>,
   options?: IntersectionObserverInit | undefined
