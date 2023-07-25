9e5bbff384e26c8bc6e81d7197b4a771c625d451
Jose Carlos Cardenas Martinez <Josenanodev@gmail.com>
2023-07-24 23:17:45 -0600

    BubbleMenu

---
+++ src/components/BubbleMenu/BubbleMenu.tsx
@@ -0,0 +1,67 @@
+import React, { Fragment, useState } from "react";
+import styles from "./BubbleMenu.module.scss";
+
+//icons
+import { BsX } from "react-icons/bs";
+import { IoMenu } from "react-icons/io5";
+
+const BubbleMenuDictionary = {
+  en: {
+    menu: "menu",
+    close: "close",
+  },
+  es: {
+    menu: "menú",
+    close: "cerrar",
+  },
+};
+
+type BubbleMenuProps = {
+  options: {
+    icon: JSX.Element;
+    text: string;
+    visible: boolean;
+    optionCallback: () => void;
+  }[];
+  language?: "en" | "es";
+};
+
+const BubbleMenu = ({ options, language = "en" }: BubbleMenuProps) => {
+  const [showMenu, setShowMenu] = useState(false);
+  return (
+    <Fragment>
+      <button className={styles["menu-button"]} onClick={() => setShowMenu(!showMenu)}>
+        {showMenu ? <BsX /> : <IoMenu />}
+        <p>
+          {showMenu
+            ? BubbleMenuDictionary[language]["close"]
+            : BubbleMenuDictionary[language]["menu"]}
+        </p>
+      </button>
+      <section
+        className={styles["menu-container"]}
+        data-status={showMenu ? "visible" : "hidden"}
+        onClick={() => setShowMenu(false)}
+      >
+        <ul>
+          {options
+            .filter((option) => option.visible)
+            .map((option, index) => (
+              <li key={option.text} style={{ transitionDelay: `${index * 100}ms` }}>
+                <button
+                  onClick={() =>
+                    setTimeout(option.optionCallback, 300 + options.length * 100)
+                  }
+                >
+                  {option.icon}
+                  {option.text}
+                </button>
+              </li>
+            ))}
+        </ul>
+      </section>
+    </Fragment>
+  );
+};
+
+export default BubbleMenu;
---
+++ src/stories/BubbleMenu.stories.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import type { Meta, StoryObj } from "@storybook/react";
+import BubbleMenu from "../components/BubbleMenu/BubbleMenu";
+import { BsHouse, BsPerson, BsSearch } from "react-icons/bs";
+
+const meta: Meta<typeof BubbleMenu> = {
+  title: "BubbleMenu",
+  component: BubbleMenu,
+  tags: ["autodocs"],
+  decorators: [
+    (Story) => (
+      <div style={{ minHeight: 400, minWidth: 360 }}>
+        <Story />
+      </div>
+    ),
+  ],
+};
+
+export default meta;
+type Story = StoryObj<typeof BubbleMenu>;
+
+export const Default: Story = {
+  args: {
+    options: [
+      {
+        icon: <BsSearch />,
+        text: "Search",
+        visible: true,
+        optionCallback: () => console.log("Search"),
+      },
+      {
+        icon: <BsPerson />,
+        text: "Profile",
+        visible: true,
+        optionCallback: () => console.log("Profile"),
+      },
+      {
+        icon: <BsHouse />,
+        text: "Home",
+        visible: true,
+        optionCallback: () => console.log("Home"),
+      },
+    ],
+  },
+};
