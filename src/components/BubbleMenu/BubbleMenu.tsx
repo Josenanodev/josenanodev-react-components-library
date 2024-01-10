158407e89229f93dcc726c400c3c4861c8d59378
Jose Carlos Cardenas Martinez <Josenanodev@gmail.com>
2024-01-09 23:05:39 -0600

    Update BubbleMenu styles and functionality

--- src/components/BubbleMenu/BubbleMenu.tsx
+++ src/components/BubbleMenu/BubbleMenu.tsx
@@ -30,7 +30,12 @@ const BubbleMenu = ({ options, language = "en" }: BubbleMenuProps) => {
   const [showMenu, setShowMenu] = useState<boolean | null>(null);
   return (
     <Fragment>
-      <button className={styles["menu-button"]} onClick={() => setShowMenu(!showMenu)}>
+      <button
+        type="button"
+        title="menu"
+        className={styles["menu-button"]}
+        onClick={() => setShowMenu(!showMenu)}
+      >
         {showMenu ? <BsX /> : <IoMenu />}
         <p>
           {showMenu
@@ -40,7 +45,9 @@ const BubbleMenu = ({ options, language = "en" }: BubbleMenuProps) => {
       </button>
       <section
         className={styles["menu-container"]}
-        data-status={showMenu === true ? "visible" : showMenu === false ? "hidden" : "idle"}
+        data-status={
+          showMenu === true ? "visible" : showMenu === false ? "hidden" : "idle"
+        }
         onClick={() => setShowMenu(false)}
       >
         <ul>
